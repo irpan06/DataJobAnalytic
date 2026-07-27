5c5ac26a5e45c3ea3c236f7866360118d6202b4f
irpan06 <irvandim8@gmail.com>
2026-07-27 10:19:30 +0700

    create dim skill table

---
+++ dbt_transform/models/gold/dim_skill.sql
@@ -0,0 +1,34 @@
+{{
+    config(
+        materialized = 'table'
+    )
+}}
+
+WITH extracted_skills AS(
+    SELECT
+        job_skills
+    FROM {{ ref('stg_job_postings') }}
+    WHERE job_skills IS NOT NULL
+      AND job_skills != '[]'
+      AND job_skills != ''
+),
+
+split_skills AS(
+    SELECT
+        TRIM(skill, " '\"") AS skill_name
+    FROM extracted_skills,
+    UNNEST(SPLIT(TRIM(job_skills, '[]'), ',')) AS skill
+),
+
+unique_skills AS(
+    SELECT DISTINCT
+        skill_name,
+    FROM split_skills
+    WHERE skill_name IS NOT NULL AND skill_name != ''
+)
+
+SELECT DISTINCT
+    FARM_FINGERPRINT(skill_name) AS skill_id,
+    skill_name
+FROM unique_skills
+ORDER BY skill_name
